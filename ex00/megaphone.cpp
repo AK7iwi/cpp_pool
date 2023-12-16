ff6a3461c7be139719584174e426c27b65286e13
AK7iwi <atzlts@proton.me>
2023-12-16 02:42:51 +0100

    ex01 setup

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 23:34:04 by mfeldman          #+#    #+#             */
-/*   Updated: 2023/12/15 23:44:55 by mfeldman         ###   ########.fr       */
+/*   Updated: 2023/12/16 02:01:33 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,17 @@
 
 int main(int argc, char **argv)
 {
-	int i = 1;
-	int j; 
-
-	if(argc < 2)
+	int i;
+	int j;
+	if(argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	while (argv[i])
+	for (i = 1; i < argc; i++)
 	{
-		j = 0;
-		while (argv[i][j])
-			std::cout << (char) toupper(argv[i][j++]);
-		std::cout << " ";
-		i++;
+        for(j = 0; argv[i][j]; j++)
+            std::cout << (char)toupper(argv[i][j]);	
 	}
 	std::cout << std::endl;
 	return (0);
