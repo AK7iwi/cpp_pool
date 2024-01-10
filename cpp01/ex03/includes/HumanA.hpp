c86adf9c9bd55590320c0e85e2ad0ba66ecd2fd8
AK7iwi <ATZLTS@proton.me>
2024-01-10 13:47:57 +0100

    ex03 setup

--- cpp01/ex03/includes/HumanA.hpp
+++ cpp01/ex03/includes/HumanA.hpp
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   HumanA.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/10 12:46:53 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/01/10 13:14:32 by mfeldman         ###   ########.fr       */
+/*   Created: 2024/01/10 13:45:34 by mfeldman          #+#    #+#             */
+/*   Updated: 2024/01/10 13:45:35 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-int main()
-{
-    std::string str = "HI THIS IS BRAIN";
-    std::string *stringPTR = &str;
-    std::string &stringREF = str;
-
-    std::cout << &str << std::endl;
-    std::cout << stringPTR << std::endl;
-    std::cout << &stringREF << std::endl;
-
-    std::cout << str << std::endl;
-    std::cout << *stringPTR << std::endl;
-    std::cout << stringREF << std::endl;
-}
--- cpp01/ex03/srcs/Weapon.cpp
+++ cpp01/ex03/srcs/Weapon.cpp
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/10 12:46:53 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/01/10 13:14:32 by mfeldman         ###   ########.fr       */
+/*   Created: 2024/01/10 13:45:41 by mfeldman          #+#    #+#             */
+/*   Updated: 2024/01/10 13:45:42 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-int main()
-{
-    std::string str = "HI THIS IS BRAIN";
-    std::string *stringPTR = &str;
-    std::string &stringREF = str;
-
-    std::cout << &str << std::endl;
-    std::cout << stringPTR << std::endl;
-    std::cout << &stringREF << std::endl;
-
-    std::cout << str << std::endl;
-    std::cout << *stringPTR << std::endl;
-    std::cout << stringREF << std::endl;
-}
