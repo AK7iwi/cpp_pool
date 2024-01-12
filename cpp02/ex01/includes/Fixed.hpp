62e4dcd2195dd470d7adceabf0ee8902c100ed9a
AK7iwi <ATZLTS@proton.me>
2024-01-12 13:58:58 +0100

    cpp02/ex02 start

---
+++ cpp02/ex01/includes/Fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 13:45:13 by mfeldman          #+#    #+#             */
+/*   Updated: 2024/01/12 13:57:53 by mfeldman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const Fixed &cpy);
+		Fixed &operator=(const Fixed &rhs);
+		virtual ~Fixed();
+
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		int toInt( void ) const;
+		float toFloat( void ) const;
+		
+		
+	private:
+		int _fixed_point;
+		static const int _store;
+};
---
+++ cpp02/ex01/srcs/Fixed.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 13:45:18 by mfeldman          #+#    #+#             */
+/*   Updated: 2024/01/12 13:58:33 by mfeldman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+#include "Fixed.hpp"
+
+const int Fixed::_store = 8;
+
+Fixed::Fixed(): _fixed_point(0) 
+{ 
+	std::cout << "Default constructor called" << std::endl;
+}
+	
+Fixed::Fixed(const Fixed &cpy)
+{ 
+	_fixed_point = cpy._fixed_point;
+	std::cout << "Copy constructor called" << std::endl;
+	cpy.getRawBits();
+}
+
+Fixed	&Fixed::operator=(const Fixed &rhs)
+{	
+	if (this != &rhs)
+	{ 
+		_fixed_point = rhs._fixed_point;
+		std::cout << "Copy assignment operator called" << std::endl;
+		rhs.getRawBits();
+	}
+	return (*this);
+}
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+
+int		Fixed::getRawBits( void ) const 
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return(_fixed_point); 
+}
+
+void	Fixed::setRawBits( int const raw ) { _fixed_point = raw ;}
