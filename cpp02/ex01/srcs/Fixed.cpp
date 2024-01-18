1d8fbb2c60cc9fd53fc8d9feabdc48b797c7871e
AK7iwi <ATZLTS@proton.me>
2024-01-18 12:10:36 +0100

    Norme

--- cpp02/ex01/srcs/Fixed.cpp
+++ cpp02/ex01/srcs/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 13:45:18 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/01/17 15:56:53 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/01/18 11:56:29 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,14 @@ const int Fixed::_nb_fractional_bits = 8;
 
 Fixed::Fixed(): _fixed_point(0) { std::cout << "Default constructor called" << std::endl; }
 
-Fixed::Fixed(const int &integer): _fixed_point(integer) 
-{ 
-	std::cout << "Int constructor called" << std::endl;
-}
+Fixed::Fixed(const int &integer): _fixed_point(integer << _nb_fractional_bits) { std::cout << "Int constructor called" << std::endl; }
 
-Fixed::Fixed(const float &floater): _fixed_point(floater) 
-{ 
-	std::cout << "Float constructor called" << std::endl;
-}
+Fixed::Fixed(const float &floater): _fixed_point(static_cast<int>(roundf(floater * (1 << _nb_fractional_bits)))) { std::cout << "Float constructor called" << std::endl; }
 
 Fixed::Fixed(const Fixed &cpy)
 { 
-	_fixed_point = cpy._fixed_point;
 	std::cout << "Copy constructor called" << std::endl;
+	_fixed_point = cpy._fixed_point;
 }
 
 Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
@@ -41,7 +35,7 @@ Fixed	&Fixed::operator=(const Fixed &rhs)
 	return (*this);
 }
 
-int		Fixed::getRawBits( void ) const 
+int		Fixed::getRawBits( void ) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
 	return (_fixed_point); 
@@ -49,17 +43,11 @@ int		Fixed::getRawBits( void ) const
 
 void	Fixed::setRawBits( int const raw ) { _fixed_point = raw ;}
 
-int Fixed::toInt( void ) const
-{
-	return (_fixed_point >> _nb_fractional_bits);
-}
+int Fixed::toInt( void ) const { return (_fixed_point >> _nb_fractional_bits); }
 
-float Fixed::toFloat( void ) const
-{
-	return (roundf(static_cast<float>(_fixed_point) / (1 << _nb_fractional_bits)));
-}
+float Fixed::toFloat( void ) const { return (static_cast<float>(_fixed_point) / (1 << _nb_fractional_bits)); }
 
-std::ostream& operator<<( std::ostream& os, Fixed const& i ) 
+std::ostream& operator<<( std::ostream& os, Fixed const& i )
 {
 	os << i.toFloat();
 	return os;
