586226c04915405c03c414dd31dd100c6be14897
AK7iwi <ATZLTS@proton.me>
2024-01-22 16:46:26 +0100

    Try to add virtual

--- cpp03/ex02/includes/ClapTrap.hpp
+++ cpp03/ex02/includes/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 18:26:47 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/01/20 22:16:53 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/01/22 16:45:28 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ class ClapTrap
     public:
         ClapTrap(std::string const &name);
         ClapTrap(ClapTrap const &cpy);
-        ~ClapTrap();
+        virtual ~ClapTrap();
 		ClapTrap&	operator=(ClapTrap const &rhs);
 
-        void    attack(const std::string& target);
+        virtual	void    attack(const std::string& target);
         void    takeDamage(unsigned int amount);
         void    beRepaired(unsigned int amount);
         
--- cpp03/ex03/includes/ClapTrap.hpp
+++ cpp03/ex03/includes/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 18:26:47 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/01/20 22:15:24 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/01/22 16:45:42 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ class ClapTrap
     public:
         ClapTrap(std::string const &name);
         ClapTrap(ClapTrap const &cpy);
-        ~ClapTrap();
+        virtual ~ClapTrap();
 		ClapTrap&	operator=(ClapTrap const &rhs);
 
-        void    attack(const std::string& target);
-        void    takeDamage(unsigned int amount);
-        void    beRepaired(unsigned int amount);
+        virtual void	attack(const std::string& target);
+        void	takeDamage(unsigned int amount);
+        void	beRepaired(unsigned int amount);
         
     protected:
         std::string     _name;
