4bd1dc32d8764dd4e6e61d9bbc03c4d483294e6d
AK7iwi <ATZLTS@proton.me>
2024-02-01 13:02:56 +0100

    Interface header created

--- cpp04/ex03/includes/ICharacter.hpp
+++ cpp04/ex03/includes/ICharacter.hpp
@@ -1,27 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/27 16:50:19 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/02/01 12:50:46 by mfeldman         ###   ########.fr       */
+/*   Created: 2024/02/01 13:01:08 by mfeldman          #+#    #+#             */
+/*   Updated: 2024/02/01 13:01:22 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
-class AMateria
-{
-
-    public:
-        AMateria(std::string const &type);
-        
-    	std::string const & getType() const; //Returns the materia type
-		
-    	virtual AMateria* clone() const = 0;
-    	virtual void use(ICharacter& target);
-    
-	protected:
-};
