6dd57d998165786e6bb2dabb65cd6d30765f3830
AK7iwi <ATZLTS@proton.me>
2024-03-20 13:54:05 +0100

    ex00 norme + cpp05 folder created

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 23:34:04 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/02/05 13:34:02 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/03/20 13:52:44 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,8 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	for (int i = 1; i < argc; i++)
-	{
         for (int j = 0; argv[i][j]; j++)
-            std::cout << (char)std::toupper(argv[i][j]);	
-	}
+            std::cout << (char)std::toupper(argv[i][j]);
 	std::cout << std::endl;
 	return (0);
 }
