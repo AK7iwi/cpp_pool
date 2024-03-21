6bfeb33043a7e86f9122887510839d5d7eeccfc9
AK7iwi <ATZLTS@proton.me>
2024-03-21 04:43:38 +0100

    cpp05 OK

--- cpp05/ex00/includes/Bureaucrat.hpp
+++ cpp05/ex00/includes/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 00:28:04 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/03/21 03:38:05 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/03/21 04:39:35 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,16 @@ class Bureaucrat
 		Bureaucrat(const Bureaucrat &cpy);
 		Bureaucrat &operator=(const Bureaucrat &cpy);
 
-		class GradeTooHighException : public std::exception {
+		class grade_too_high_exception : public std::exception 
+		{
 			public:
-				std::string	tooHigh() const throw();
+				std::string	too_high() const throw();
 		};
 
-		class GradeTooLowException : public std::exception {
+		class grade_too_low_exception : public std::exception {
+			
 			public:
-				std::string	tooLow() const throw();
+				std::string	too_low() const throw();
 		};
 
 		std::string	get_name() const;
--- cpp05/ex00/srcs/Bureaucrat.cpp
+++ cpp05/ex00/srcs/Bureaucrat.cpp
@@ -6,11 +6,38 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 00:27:52 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/03/21 03:49:07 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/03/21 04:41:49 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name)
+{
+	if (grade < 1)
+		throw grade_too_high_exception();
+	else if (grade > 150)
+		throw grade_too_low_exception();
+	_grade = grade;
+	std::cout <<"Bureaucrat default constructor called"<< std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &cpy) 
+{
+    std::cout << "Bureaucrat copy constructor called " <<std::endl;
+    _grade = cpy._grade;
+}
+
+Bureaucrat::~Bureaucrat() {std::cout << "Bureaucrat destructor called" << std::endl;}
+
+
+Bureaucrat&	Bureaucrat::operator=(Bureaucrat const &rhs) 
+{	
+    std::cout << "Bureaucrat copy assignement operator called" << std::endl;
+	if (this != &rhs)
+		_grade = rhs._grade;
+	return (*this);
+}
 
 std::string Bureaucrat::get_name() const {return(_name);}
 
@@ -21,7 +48,7 @@ void Bureaucrat::increment_grade()
     if (_grade > 1)
         _grade--;
     else
-        throw GradeTooHighException();
+        throw grade_too_high_exception();
 }
 
 void Bureaucrat::decrement_grade() 
@@ -29,8 +56,12 @@ void Bureaucrat::decrement_grade()
     if (_grade < 150)
         _grade++;
     else
-        throw GradeTooLowException();
+        throw grade_too_low_exception();
 }
+
+std::string	Bureaucrat::grade_too_high_exception::too_high() const throw() {return ("Bureaucrat grade is too high");}
+
+std::string	Bureaucrat::grade_too_low_exception::too_low() const throw(){return ("Bureaucrat grade is too low");}
 
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat) 
 {
