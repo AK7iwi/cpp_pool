4bcae8d88cf63fccedeee464e31a054147daea17
AK7iwi <ATZLTS@proton.me>
2024-03-21 05:18:57 +0100

    main cpp05/ex00 OK

--- cpp05/ex00/srcs/main.cpp
+++ cpp05/ex00/srcs/main.cpp
@@ -6,13 +6,100 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 00:28:08 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/03/21 00:47:57 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/03/21 05:18:24 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-int main()
+int main(void)
 {
-	
+	try 
+	{
+		std::cout<< "\nTest #1 : Create Bureaucrat A with grade 0"<< std::endl;
+		Bureaucrat A("A", 0);
+	}
+	catch(Bureaucrat::grade_too_high_exception &e) 
+	{
+		std::cout << e.too_high() << std::endl;
+	} 
+	catch(Bureaucrat::grade_too_low_exception &e)
+	{
+		std::cout << e.too_low() << std::endl;
+	}
+	try 
+	{
+		std::cout <<"\nTest #2 : Create Bureaucrat B with grade 151" << std::endl;
+		Bureaucrat B("B", 151);
+	}
+	catch(Bureaucrat::grade_too_high_exception &e) 
+	{
+		std::cout << e.too_high() << std::endl;
+	} 
+	catch(Bureaucrat::grade_too_low_exception &e) 
+	{
+		std::cout << e.too_low() << std::endl;
+	}
+	try 
+	{
+		std::cout << "\nTest #3 : Create Bureaucrat C with grade 1"<< std::endl;
+		Bureaucrat C("C", 1);
+		std::cout << C << std::endl;
+		std::cout << "Test #4 : Bureaucrat C decrement grade" << std::endl;
+		C.decrement_grade();
+		std::cout << C << std::endl;
+		std::cout << "Test #5 : Bureaucrat C increment grade" << std::endl;
+		C.increment_grade();
+		std::cout << C << std::endl;
+		std::cout << "Test #6 : Bureaucrat C increment grade" << std::endl;
+		C.increment_grade();
+	} 
+	catch(Bureaucrat::grade_too_high_exception &e) 
+	{
+		std::cout << e.too_high() << std::endl;
+	} 
+	catch(Bureaucrat::grade_too_low_exception &e) 
+	{
+		std::cout << e.too_low()  << std::endl;
+	}
+	try 
+	{
+		std::cout << "\nTest #7 : Create Bureaucrat D with grade 150" << std::endl;
+		Bureaucrat D("D", 150);
+		std::cout << D << std::endl;
+		std::cout << "Test #8 : Bureaucrat D increment grade" << std::endl;
+		D.increment_grade();
+		std::cout << D << std::endl;
+		std::cout << "Test #9 : Bureaucrat D decrement grade" << std::endl;
+		D.decrement_grade();
+		std::cout << D << std::endl;
+		std::cout << "Test #10 : Bureaucrat D decrement grade" << std::endl;
+		D.decrement_grade();
+	} 
+	catch(Bureaucrat::grade_too_high_exception &e) 
+	{
+		std::cout << e.too_high() << std::endl;
+	} 
+	catch(Bureaucrat::grade_too_low_exception &e) 
+	{
+		std::cout  << e.too_low()  << std::endl;
+	}
+	try 
+	{
+		std::cout << "\nTest #11 : Create Bureaucrat E with grade 100"  << std::endl;
+		Bureaucrat E("E", 100);
+		std::cout << E << std::endl;
+		std::cout << "Test #12 : Create Bureaucrat F via copy of Bureaucrat E" << std::endl;
+		Bureaucrat F(E);
+		std::cout << F << std::endl;
+	} 
+	catch(Bureaucrat::grade_too_high_exception &e) 
+	{
+		std::cout  << e.too_high() << std::endl;
+	} 
+	catch(Bureaucrat::grade_too_low_exception &e) 
+	{
+		std::cout << e.too_low() << std::endl;
+	}
+	return (0);
 }
