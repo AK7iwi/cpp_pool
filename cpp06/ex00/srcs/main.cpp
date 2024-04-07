275bdd4e5d362b19b10a458051312a4b81eecb29
AK7iwi <ATZLTS@proton.me>
2024-04-07 13:27:36 +0200

    Struc of the code

--- cpp06/ex00/srcs/main.cpp
+++ cpp06/ex00/srcs/main.cpp
@@ -6,12 +6,11 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 07:08:14 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/04/06 05:58:51 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/04/06 17:58:34 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
-
 
 int main(int argc, char **argv) 
 {
