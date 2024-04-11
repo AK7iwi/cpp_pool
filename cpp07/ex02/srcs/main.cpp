15412a27a213cdb775a5ee7760dc120734a596e8
AK7iwi <ATZLTS@proton.me>
2024-04-11 22:43:14 +0200

    MAIN

--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -6,13 +6,50 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 21:10:53 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/04/11 21:26:54 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/04/11 22:43:05 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int main()
+int main() 
 {
-	
+	try 
+	{
+        // Testing default constructor
+        Array<int> empty_array;
+        std::cout << "Size of emptyArray: " << empty_array.size() << std::endl;
+
+        // Testing constructor with size parameter
+        Array<int> int_array(5);
+        for (unsigned int i = 0; i < int_array.size(); ++i)
+            int_array[i] = i + 1;
+
+        // Testing copy constructor and assignment operator
+        Array<int> copied_array(int_array);
+        Array<int> assigned_array;
+        assigned_array = int_array;
+
+        // Modifying original array
+        int_array[0] = 100;
+
+        // Displaying elements of copied and assigned arrays
+        std::cout << "Copied array elements: ";
+        for (unsigned int i = 0; i < copied_array.size(); ++i)
+            std::cout << copied_array[i] << " ";
+        std::cout << std::endl;
+
+        std::cout << "Assigned array elements: ";
+        for (unsigned int i = 0; i < assigned_array.size(); ++i)
+            std::cout << assigned_array[i] << " ";
+        std::cout << std::endl;
+
+        // Testing out of bounds access
+        std::cout << "Accessing out of bounds element: ";
+        std::cout << int_array[10] << std::endl; // This should throw an exception
+    } 
+	catch (const std::exception& e) 
+	{std::cerr << "Exception caught: " << e.what() << std::endl;}
+
+    return (0);
 }
