720263db79d415f91584badb73054694ab6a9fb0
AK7iwi <ATZLTS@proton.me>
2024-04-17 21:51:30 +0200

    Some print test

--- cpp09/ex00/srcs/BitcoinExchange.cpp
+++ cpp09/ex00/srcs/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 17:25:10 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/04/17 21:22:47 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/04/17 21:50:49 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,20 @@ void BitcoinExchange::parse_line(std::string &line)
 {
 	std::istringstream	iss(line);
 	std::string	date, value_str;
-	std::getline(iss, date, '|');
+	std::getline(iss, date, '|'); // check that it exist 
 	std::getline(iss, value_str);
 	// float value = std::stof(value_str);
 	
+	date.erase(0, date.find_first_not_of(" \t"));
+    date.erase(date.find_last_not_of(" \t") + 1);
+
+    value_str.erase(0, value_str.find_first_not_of(" \t"));
+    value_str.erase(value_str.find_last_not_of(" \t") + 1);
+
 	std::cout << date << std::endl;
 	std::cout << value_str << std::endl;
 
-	//erase 
 
-	if (line.empty())
-	{
-		std::cerr << "Empty line" << std::endl;
-		return ;
-	}
 	//check date
 	//check |
 	//check value
@@ -64,11 +64,13 @@ void BitcoinExchange::display_result()
     if (!input_file.is_open()) 
 		throw(std::invalid_argument("Error: Unable to open input file"));
 
-	while(getline(input_file, line))
+	while(getline(input_file, line)) // skip the first line 
 	{
+		if (line.empty())
+			throw(std::invalid_argument("Empty line"));
 		// std::cout << line << std::endl;
 		parse_line(line); 
-		// find_in_db()
+		// find_date_in_db_and_get_value()
 		// exchange_value();
 		// display();
 	}
