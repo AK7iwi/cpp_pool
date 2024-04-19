8f4cffae414cb383233e029f03e65919a8836b45
AK7iwi <ATZLTS@proton.me>
2024-04-19 21:18:38 +0200

    is_valid_value

--- cpp09/ex00/srcs/BitcoinExchange.cpp
+++ cpp09/ex00/srcs/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: mfeldman <mfeldman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 17:25:10 by mfeldman          #+#    #+#             */
-/*   Updated: 2024/04/17 22:25:20 by mfeldman         ###   ########.fr       */
+/*   Updated: 2024/04/19 21:13:38 by mfeldman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,22 @@ BitcoinExchange&	BitcoinExchange::operator=(BitcoinExchange const &rhs)
 	return(*this);
 }
 
-bool is_valid_date(std::string &date)
-{
+// void is_valid_date(std::string &date)
+// {
 	
-}
+// }
 
 
-bool is_valid_value(std::string &value)
+void is_valid_value(std::string &value)
 {
+	float value_f;
+
+	std::istringstream(value) >> value_f;
 	
+	if (value_f > 1000 || value_f < 0)
+		std::cout << "Error: not a positive number or too large number" << std::endl;
+		
+	//is_digit test
 }
 
 void BitcoinExchange::parse_line(std::string &line)
@@ -47,23 +54,23 @@ void BitcoinExchange::parse_line(std::string &line)
 	std::string	date, value_str;
 
 	if (!std::getline(iss, date, '|') || !std::getline(iss, value_str))
-		throw(std::invalid_argument("Invalid line"));
-	// float value = std::stof(value_str);
-	
+		std::cout << "Error: invalid format line" << std::endl;
+
 	date.erase(0, date.find_first_not_of(" \t"));
     date.erase(date.find_last_not_of(" \t") + 1);
 
     value_str.erase(0, value_str.find_first_not_of(" \t"));
     value_str.erase(value_str.find_last_not_of(" \t") + 1);
+	
+	// is_valid_date(date);
+	is_valid_value(value_str);
 
-	is_valid_date(date);
-	is_valid_value(date);
-	//check value
-	//check int max/min, positiv number
-	std::cout << date << std::endl;
-	std::cout << value_str << std::endl;
-
+	// if (is_valid_value(value_str) && is_valid_date(date))
 	// _bitcoin_data[date] = value;
+	
+	// std::cout << date << std::endl;
+	// std::cout << value_str << std::endl;
+
 }
 
 void BitcoinExchange::display_result()
@@ -72,13 +79,12 @@ void BitcoinExchange::display_result()
 	std::string line;
     
     if (!input_file.is_open()) 
-		throw(std::invalid_argument("Error: Unable to open input file"));
+		throw (std::invalid_argument("Error: could to open input file"));
 
-	while(getline(input_file, line)) // skip the first line 
+	while(getline(input_file, line)) // skip the first line, get_line one time before the while
 	{
 		if (line.empty())
-			throw(std::invalid_argument("Empty line"));
-		// std::cout << line << std::endl;
+			std::cout << "Error: empty line" << std::endl;
 		parse_line(line); 
 		// find_date_in_db_and_get_value()
 		// exchange_value();
